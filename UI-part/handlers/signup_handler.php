e6f6ba2cc40bf8cb3fd096f8064e1bc52954c1e9
Xiaolong <long56336@gmail.com>
2016-10-23 21:17:31 -0600

    Update signup_handler.php

--- UI-part/handlers/signup_handler.php
+++ UI-part/handlers/signup_handler.php
@@ -6,7 +6,7 @@ $mysqli = new mysqli("localhost", "admin", "admin", "vcard");
         if ($mysqli->connect_errno) {
          echo "Failed to connect to MySQL: " .$mysqli->connect_error;
         }else{
-                echo "Success!";
+                //echo "Success!";
                 }
 
 $email = $mysqli->real_escape_string($_POST['email']);
@@ -32,11 +32,11 @@ $suc = $_SESSION['account']->save();
 print_r($suc);
 if($suc['success'] == true){
       $_SESSION['alert'] = new Alert("Congratulations and Welcome!", "succuss");
-    header("Location: login.php");
+    header("Location: ../login.php");
 }else{
     
      $_SESSION['alert'] = new Alert("Your registration are failed", "Warning");
-    header("Location: signup.php");
+    header("Location: ../signup.php");
 }
 ?>
 
