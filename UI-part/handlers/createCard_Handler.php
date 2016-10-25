246d83bc4af2a56e96b6547258e2a4c9557b787e
Sean <sbrough89@gmail.com>
2016-10-25 02:25:41 +0000

    UPdated createCard_Handler

--- UI-part/handlers/createCard_Handler.php
+++ UI-part/handlers/createCard_Handler.php
@@ -1,12 +1,14 @@
 <? php
 
+session_start();
+
 $card = new Card($_POST["first_name"], $_POST["last_name"], $_POST["position_name"], $_POST["business_name"],
                  $_POST["business_address"], $_POST["city"], $_POST["state"], $_POST["zipcode"], $_POST["business_website"]
-                 $_POST["email"], $_POST["phone_number"], $Session["$user_id"])
+                 $_POST["email"], $_POST["phone_number"], $_SESSION["user_id"])
                  
-Card::save_card();
+$save_card = $card->save_card();
 
-if (Card::save_card() === TRUE) {
+if ($save_card === TRUE) {
     header("Location: yourcards.php");
 } else {
 header("Location: create_card.php");
